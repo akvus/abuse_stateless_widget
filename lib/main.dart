1a9b57c13ae3e5ce0441b9af72d090341b878386
akvus <mr.akvus@gmail.com>
2022-09-18 17:21:47 +0200

    Add example of abusing Stateful's setState

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  late final _element = StatelessElement(this);
-
-  var counter = 0;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  StatelessElement createElement() => _element;
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +25,50 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: InkWell(
-            child: Text("Nothing - $counter",
-                style: TextStyle(
-                  fontSize: 20,
-                )),
-            onTap: () {
-              counter++;
-              print(counter);
-              _element.markNeedsBuild();
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Button(),
+              InkWell(
+                  child: MyWidget(counter: _counter),
+                  onTap: () {
+                    setState(() {
+                      _counter++;
+                    });
+                  }),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Text('Click me'),
+      onTap: () {
+        final state = context.findAncestorStateOfType<_MyAppState>()!;
+        state.setState(() {
+          state._counter = 12;
+        });
+      },
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key, required this.counter});
+
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello, World! $counter',
+      style: Theme.of(context).textTheme.headline4,
     );
   }
 }
